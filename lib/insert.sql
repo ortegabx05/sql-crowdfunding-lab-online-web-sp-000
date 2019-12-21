e6f99659408abc351a7ff13ec4431a3295d77365
jessi ortega <ortegabx05@gmail.com>
2019-12-21 04:59:39 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Take Care', 'Music', 220000, '9/5/20', '12/1/20'),
+('GKMC', 'Music', 330000, '4/5/20', '8/8/20'),
+('Friday', 'Movie', 7000, '6/25/20', '3/20/20'),
+('Goosebumps', 'Book', 90000, '1/7/20', '10/10/20'),
+('Friday Lights', 'Music', 11000, '9/29/20', '5/8/20'),
+('Marvel', 'Movie', 80000, '12/22/20', '11/21/20'),
+('Jesus is King', 'Music', 4000, '9/11/20', '3/2/20'),
+('Harry Potter', 'Book', 120000, '8/16/20', '9/23/20'),
+('Fun', 'Music', 8000, '9/12/20', '12/25/20'),
+('Carter', 'Music', 17000, '7/27/20', '8/13/20');
+
+INSERT INTO users (name, age) VALUES
+('Jessi', 30),
+('Rickey',26),
+('David', 27),
+('Sanford', 18),
+('Willis', 21),
+('Eric', 17),
+('Jeremy', 22),
+('Natasia', 24),
+('Ashley', 32),
+('Angie', 20),
+('Mariana', 29),
+('sara', 27),
+('Maxi', 19),
+('Alexa', 28),
+('Karen', 33),
+('Stephanie', 27),
+('Renard', 26),
+('Leo', 23),
+('Nick', 36),
+('Andy', 40);
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES(100, 2, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(250, 1, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(500, 1, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(300, 2, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(100, 3, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(50, 4, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(200, 5, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(150 6, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(350, 7, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(400, 8, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(250, 8, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(150, 9, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(300, 9, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(250, 10, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(500, 12, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(1000, 11, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(800, 13, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(600, 20, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(100, 8, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(250, 18, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(250, 12, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(200, 14, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(300, 20, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(500, 19, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(400, 17, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(100, 16, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(300, 10, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(220, 11, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(150, 17, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES(200, 16, 7);
